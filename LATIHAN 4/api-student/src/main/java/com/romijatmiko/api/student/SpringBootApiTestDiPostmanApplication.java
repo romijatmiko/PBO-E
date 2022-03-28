119d581b9789345be5a553a76c6766b8900a61da
Romi Jatmiko <romijatmiko033@gmail.com>
2022-03-28 21:44:45 +0700

    Mantaapppp

---
+++ LATIHAN 4/api-student/src/main/java/com/romijatmiko/api/student/SpringBootApiTestDiPostmanApplication.java
@@ -0,0 +1,13 @@
+package com.romijatmiko.api.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiTestDiPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiTestDiPostmanApplication.class, args);
+	}
+
+}
